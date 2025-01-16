94fc429bc59f2646de89db001b4e78ee6fbbacfe
KMosunov <kmosunov@gmail.com>
2025-01-16 16:09:02 +0300

    fix/km/.yml files changed

--- tests/HW_09/orderById.spec.ts
+++ tests/HW_09/orderById.spec.ts
@@ -1,7 +1,48 @@
 import { expect, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 
-test('Successful changing order details with correct id. Status 200', async ({ request }) => {
+test('DEL Successful deleting order with correct id. Status 204', async ({ request }) => {
+  // Test data: 1,2,5,9,10
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  console.log('response status: ', response.status())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.NO_CONTENT)
+})
+
+test('DEL Unsuccessful with incorrect id. Status 400', async ({ request }) => {
+  // Test data: 0,11
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/0', {
+    headers: requestHeaders,
+  })
+  console.log('response status: ', response.status())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+test('DEL Unsuccessful with empty id. Status 400', async ({ request }) => {
+  // Test data: 0,11
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/', {
+    headers: requestHeaders,
+  })
+  console.log('response status: ', response.status())
+  console.log('response headers: ', response.headers())
+  //BUG-006 вместо 400 программа возвращает 405
+  //expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+  expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+})
+
+test('PUT Successful changing order details with correct id. Status 200', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -24,7 +65,7 @@ test('Successful changing order details with correct id. Status 200', async ({ r
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
-test('Successful with correct id and status ACCEPTED. Status 200', async ({ request }) => {
+test('PUT Successful with correct id and status ACCEPTED. Status 200', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -48,7 +89,7 @@ test('Successful with correct id and status ACCEPTED. Status 200', async ({ requ
   expect(requestBody.status).toBe('ACCEPTED')
 })
 
-test('Successful with correct id and status INPROGRESS. Status 200', async ({ request }) => {
+test('PUT Successful with correct id and status INPROGRESS. Status 200', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -72,7 +113,7 @@ test('Successful with correct id and status INPROGRESS. Status 200', async ({ re
   expect(requestBody.status).toBe('INPROGRESS')
 })
 
-test('Successful with correct id and status DELIVERED. Status 200', async ({ request }) => {
+test('PUT Successful with correct id and status DELIVERED. Status 200', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -96,7 +137,7 @@ test('Successful with correct id and status DELIVERED. Status 200', async ({ req
   expect(requestBody.status).toBe('DELIVERED')
 })
 
-test('Unsuccessful with incorrect customerName. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with incorrect customerName. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -123,7 +164,7 @@ test('Unsuccessful with incorrect customerName. Status 400', async ({ request })
   expect(requestBody.customerName).not.toBe(Number)
 })
 
-test('Unsuccessful with empty customerName. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with empty customerName. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -150,7 +191,7 @@ test('Unsuccessful with empty customerName. Status 400', async ({ request }) => 
   //expect(requestBody.customerName).not.toBe('')
 })
 
-test('Unsuccessful with incorrect customerPhone. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with incorrect customerPhone. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -177,7 +218,7 @@ test('Unsuccessful with incorrect customerPhone. Status 400', async ({ request }
   //expect(requestBody.customerPhone).toBe(String)
 })
 
-test('Unsuccessful with empty customerPhone. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with empty customerPhone. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -204,7 +245,7 @@ test('Unsuccessful with empty customerPhone. Status 400', async ({ request }) =>
   //expect(requestBody.customerPhone).toBe(String)
 })
 
-test('Unsuccessful with empty comment. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with empty comment. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -230,7 +271,7 @@ test('Unsuccessful with empty comment. Status 400', async ({ request }) => {
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
-test('Unsuccessful with incorrect id. Status 400', async ({ request }) => {
+test('PUT Unsuccessful with incorrect id. Status 400', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -253,7 +294,7 @@ test('Unsuccessful with incorrect id. Status 400', async ({ request }) => {
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-test('Unsuccessful with empty id. Status 405 Method Nit Allowed', async ({ request }) => {
+test('PUT Unsuccessful with empty id. Status 405 Method Nit Allowed', async ({ request }) => {
   const requestHeaders = {
     api_key: '1234567890123456',
   }
@@ -274,4 +315,42 @@ test('Unsuccessful with empty id. Status 405 Method Nit Allowed', async ({ reque
   console.log('response body: ', await response.json())
   console.log('response headers: ', response.headers())
   expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+})
+
+test('GET Successful getting order details with correct id. Status 200', async ({ request }) => {
+  // Test data: 1,2,5,9,10
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/2')
+  // Log the response status, body and headers
+  console.log('response status: ', response.status())
+  console.log('response body: ', await response.json())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('GET Unsuccessful with incorrect id. Status 400', async ({ request }) => {
+  // Test data: 0,11
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
+  // Log the response status, body and headers
+  console.log('response status: ', response.status())
+  console.log('response body: ', await response.json())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+test('GET Unsuccessful with empty id. Status 500', async ({ request }) => {
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/')
+  // Log the response status, body and headers
+  console.log('response status: ', response.status())
+  console.log('response body: ', await response.json())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+})
+
+test('GET Unsuccessful with id string format. Status 400', async ({ request }) => {
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/fgfg')
+  // Log the response status, body and headers
+  console.log('response status: ', response.status())
+  console.log('response body: ', await response.json())
+  console.log('response headers: ', response.headers())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
