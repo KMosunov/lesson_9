a50c6fffd8550ca54f4e3ad5badf3a11b230ec25
KMosunov <kmosunov@gmail.com>
2025-01-28 22:43:42 +0300

    feat | ...

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,13 +2,12 @@ import { expect, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 import { LoginDTO } from './DTO/LoginDTO'
 
-test.describe ("Login tests", async  () => {
-  test ('Successful authorization', async  ({ request }) => {
-    const response = await request.post ('https://backend.tallinn-learning.ee/login/student', {
-      data: LoginDTO.createLoginWithCorrectData()
-    });
-    console.log((await response.text()))
-    expect (response.status()).toBe(StatusCodes.OK);
+test.describe('Login tests', async () => {
+  test('Successful authorization', async ({ request }) => {
+    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
+    console.log(await response.text())
+    expect(response.status()).toBe(StatusCodes.OK)
   })
-
 })
