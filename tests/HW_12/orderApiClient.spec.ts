aeb9a99d373e879b617619e7ba248a7103cee230
KMosunov <kmosunov@gmail.com>
2025-02-06 11:41:06 +0300

    feat | API Client using tests added

--- tests/HW_12/orderApiClient.spec.ts
+++ tests/HW_12/orderApiClient.spec.ts
@@ -7,7 +7,6 @@ test.describe('Order tests API Client', async () => {
   test('TL-12-1 API Client Successful authorization', async ({ request }) => {
     const apiClient = await ApiClient.getInstance(request)
     expect(apiClient.jwt).not.toBeUndefined()
-
     expect(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(apiClient.jwt)).toBeTruthy()
   })
 
@@ -23,13 +22,9 @@ test.describe('Order tests API Client', async () => {
     expect(responseCreateOrder.status()).toBe(StatusCodes.OK)
   })
 
-  test('TL-12-3 API Successful authorization, order creation and order status', async ({
-    request,
-  }) => {
+  test('TL-12-3 API Successful authorization, order creation and order status', async ({request}) => {
     const apiClient = await ApiClient.getInstance(request)
-    const responseOrderStatus = await request.get(
-      `https://backend.tallinn-learning.ee/orders/${await apiClient.createOrderAndReturnOrderId()}`,
-      {
+    const responseOrderStatus = await request.get(`https://backend.tallinn-learning.ee/orders/${await apiClient.createOrderAndReturnOrderId()}`, {
         headers: {
           Authorization: 'Bearer ' + apiClient.jwt,
         },
@@ -41,9 +36,7 @@ test.describe('Order tests API Client', async () => {
     expect(requestedOrder.status).toBe('OPEN')
   })
 
-  test('TL-12-4 API Successful authorization, order creation, order status and delete', async ({
-    request,
-  }) => {
+  test('TL-12-4 API Successful authorization, order creation, order status and delete', async ({request}) => {
     const apiClient = await ApiClient.getInstance(request)
     await apiClient.deleteOrder()
   })
