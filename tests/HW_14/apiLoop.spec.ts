6d8378e18f8ac5b28b84ca13876c9a7773688ad9
KMosunov <kmosunov@gmail.com>
2025-02-14 23:12:20 +0300

    feat | HW_14 Api loop local tests added

--- tests/HW_14/apiLoop.spec.ts
+++ tests/HW_14/apiLoop.spec.ts
@@ -4,28 +4,28 @@ import { StatusCodes } from 'http-status-codes'
 const baseURL = 'http://localhost:3000/users'
 
 test.describe('User management API with loop', () => {
-  test.beforeEach(async ({ request }) => {
-    const response = await request.get(`${baseURL}`)
-    const responseBody = await response.json()
-    const numberOfObjects = responseBody.length
-
-    const userIDs = []
-    for (let i = 0; i < numberOfObjects; i++) {
-      const userID = responseBody[i].id
-      userIDs.push(userID)
-    }
-
-    for (let i = 0; i < numberOfObjects; i++) {
-      const response = await request.delete(`${baseURL}/${userIDs[i]}`)
-      expect.soft(response.status()).toBe(StatusCodes.OK)
-    }
-
-    const responseAfterDelete = await request.get(`${baseURL}`)
-    expect(responseAfterDelete.status()).toBe(StatusCodes.OK)
-
-    const responseBodyEmpty = await responseAfterDelete.text()
-    expect(responseBodyEmpty).toBe('[]')
-  })
+  // test.beforeEach(async ({ request }) => {
+  //   const response = await request.get(`${baseURL}`)
+  //   const responseBody = await response.json()
+  //   const numberOfObjects = responseBody.length
+  //
+  //   const userIDs = []
+  //   for (let i = 0; i < numberOfObjects; i++) {
+  //     const userID = responseBody[i].id
+  //     userIDs.push(userID)
+  //   }
+  //
+  //   for (let i = 0; i < numberOfObjects; i++) {
+  //     const response = await request.delete(`${baseURL}/${userIDs[i]}`)
+  //     expect.soft(response.status()).toBe(StatusCodes.OK)
+  //   }
+  //
+  //   const responseAfterDelete = await request.get(`${baseURL}`)
+  //   expect(responseAfterDelete.status()).toBe(StatusCodes.OK)
+  //
+  //   const responseBodyEmpty = await responseAfterDelete.text()
+  //   expect(responseBodyEmpty).toBe('[]')
+  // })
 
   test('TL-14-01 GET / - should return empty when no users', async ({ request }) => {
     const response = await request.get(`${baseURL}`)
